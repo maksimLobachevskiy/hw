09a55ad409740d28572001c7398febf038507429
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2020-11-02 21:31:49 +0200

    js

--- JS HW1/js/optional.js
+++ JS HW1/js/optional.js
@@ -1,11 +1,10 @@
 let age;
-let name;
 do {
     age = parseInt(prompt("How old are you?"));
 } while (age == '' || isNaN(age));
-    do {
-        name = prompt("What is your name?");
-    } while (name == 0);
+let name = prompt("What is your name?");
+    while (name === "") name = prompt("What is your name?", name);
+
 if (age >= 18 && age <= 22) {
     let ageConf = confirm("Are you sure you want to continue?");
     if (ageConf) {
