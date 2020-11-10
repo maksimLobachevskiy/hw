209c1398cd8f94c0d10f3f5f2a248b0b283f1280
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2020-11-10 15:11:59 +0200

    opt

--- JS HW2/js/optional.js
+++ JS HW2/js/optional.js
@@ -1,10 +1,6 @@
 let m = parseInt(prompt("Введите число"));
 let n = parseInt(prompt("Введите число"));
-do {
-    m = parseInt(prompt("Введите число"));
-    n = parseInt(prompt("Введите число"));
-}
-while (isNaN(m) || isNaN(n) || m > n);
+
     primeNumber:
         for (let i = m; i <= n; i++) {
             for (j = 2; j < i; j++) {
