0096047e15390f52a127a72142dabb1ef9dd4049
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2020-11-10 16:16:52 +0200

    validate input

--- JS HW2/js/optional.js
+++ JS HW2/js/optional.js
@@ -1,5 +1,7 @@
-let m = parseInt(prompt("Введите число"));
-let n = parseInt(prompt("Введите число"));
+let m = 0;
+let n = 0;
+while (m == '' || isNaN(m)) m = parseInt(prompt("Введите число m"));
+while (n == '' || isNaN(n)) n = parseInt(prompt("Введите число n"));
 
     primeNumber:
         for (let i = m; i <= n; i++) {
