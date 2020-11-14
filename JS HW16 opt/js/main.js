91c0daf3729bc369db1fb0d6775f24a61cd2fc37
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2020-11-14 18:28:51 +0200

    fib

--- JS HW16 opt/js/main.js
+++ JS HW16 opt/js/main.js
@@ -10,10 +10,10 @@ function fib(n) {
         }
         return F1;
     } else {
-        let F0 = -1;
-        let F1 = -1;
-        for (let i = -3; i >= n; i--) {
-            let F2 = F0 + F1;
+        let F0 = 1;
+        let F1 = 1;
+        for (let i = 0; i >= n; i--) {
+            let F2 = F0 - F1;
             F0 = F1;
             F1 = F2;
         }
--- JS HW3/js/optional.js
+++ JS HW3/js/optional.js
@@ -1,11 +1,10 @@
 let numA = parseFloat(prompt("Введите число А"));
-while (numA == '' || isNaN(numA)) numA = parseFloat(prompt("Введите число А"));
+while (numA === '' || isNaN(numA)) numA = parseFloat(prompt("Введите число А"));
 let numB = parseFloat(prompt("Введите число B"));
-while (numB == '' || isNaN(numB)) numB = parseFloat(prompt("Введите число B"));
+while (numB === '' || isNaN(numB)) numB = parseFloat(prompt("Введите число B"));
 let operValue = prompt("Введите операцию +, -, *, /");
 
 function calc(firstNumber,operation,secondNumber) {
-    operation = operValue;
     switch (operation) {
         case "+":
             return firstNumber + secondNumber;
