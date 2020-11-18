e0f0dccba78dc58af6563a42415a04a1ffd58146
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2020-11-18 20:03:15 +0200

    JS HW5 2 Variant

--- JS HW5/js/main.js
+++ JS HW5/js/main.js
@@ -18,3 +18,32 @@ function createNewUser() {
 
 let newUser = new createNewUser();
 console.log(`Ваш логин: ${newUser.getLogin()}\nВаш пароль: ${newUser.getPassword()}\nВаш возраст: ${newUser.getAge()}`);
+
+// 2 Вариант решения
+/*
+function createNewUser() {
+    let fName = prompt("Enter your name");
+    let lName = prompt("Enter your surname");
+    let birth = prompt("Enter your date of birth in form: dd.mm.yyyy", "01.01.2001");
+    const newUser = {
+        firstName: fName,
+        lastName: lName,
+        birthday: birth,
+        getLogin: function () {
+            let myLogin = this.firstName.charAt(0).toLowerCase()+this.lastName.toLowerCase();
+            return myLogin;
+        },
+        getAge: function () {
+            let birthYear = this.birthday.slice(6);
+            return (new Date().getFullYear() - birthYear);
+        },
+        getPassword: function () {
+            let myPassword = this.firstName.charAt(0).toUpperCase()+this.lastName.toLowerCase()+this.birthday.slice(6);
+            return myPassword;
+        },
+    }
+    return newUser;
+}
+const user = createNewUser();
+console.log(`Ваш логин: ${user.getLogin()}\nВаш пароль: ${user.getPassword()}\nВаш возраст: ${user.getAge()}`);
+*/
