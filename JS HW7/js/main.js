9204af376b75abb618479438cd8ef72c19cb28b1
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2020-11-28 19:02:49 +0200

    JS HW7 List

---
+++ JS HW7/js/main.js
@@ -0,0 +1,20 @@
+
+function displayList(array, parentDom) {
+    const list = document.createElement("ul");
+    array.map(
+        function displayLi (currentValue) {
+            const listLi = document.createElement("li");
+            listLi.innerHTML = currentValue;
+            list.append(listLi);
+        }
+    );
+
+        if (parentDom) {
+            parentDom.append(list);
+        } else {
+            document.body.append(list);
+        }
+    }
+let arr = ["JavaScript", "JAVA", "HTML", "CSS", "C#", "C++", "Phyton", "PHP"];
+displayList(arr, content);
+
