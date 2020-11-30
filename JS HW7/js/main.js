1312dc2fd001620e995e45db28d236ed1ec6eeb0
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2020-11-30 22:47:33 +0200

    JS HW7

--- JS HW7/js/main.js
+++ JS HW7/js/main.js
@@ -1,20 +1,24 @@
-
-function displayList(array, parentDom) {
-    const list = document.createElement("ul");
-    array.map(
-        function displayLi (currentValue) {
-            const listLi = document.createElement("li");
-            listLi.innerHTML = currentValue;
-            list.append(listLi);
-        }
-
-    );
-        if (parentDom) {
-            parentDom.append(list);
-        } else {
-            document.body.append(list);
-        }
-    }
+//1. С шаблонными строками
+function displayList(array, parentDom = document.body) {
+    const elements = array.map((item) => {
+        return `<li>${item}</li>`;
+    });
+    parentDom.innerHTML = `<ul>${elements.join(" ")}</ul>`;
+}
 let arr = ["JavaScript", "JAVA", "HTML", "CSS", "C#", "C++", "Phyton", "PHP"];
-displayList(arr, content);
+displayList(arr, document.querySelector('#content'));
 
+//2. Без шаблонных строк
+/*function displayList(array, parentDom = document.body) {
+    const elements = array.map((item) => {
+            const listLi = document.createElement("li");
+            listLi.innerHTML = item;
+            return listLi;
+        });
+
+    const list = document.createElement("ul");
+    list.append(...elements);
+    parentDom.append(list);
+}
+let arr = ["JavaScript", "JAVA", "HTML", "CSS", "C#", "C++", "Phyton", "PHP"];
+displayList(arr, document.querySelector('#content'));*/
