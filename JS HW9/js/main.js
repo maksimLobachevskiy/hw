af412290e11b3a02fcc3e62a086111279692c291
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2020-12-01 01:51:47 +0200

    JS HW9

--- JS HW9/js/main.js
+++ JS HW9/js/main.js
@@ -1,25 +1,13 @@
-function tab () {
-    let links = document.querySelectorAll('.tabs-title');
-    let content = document.querySelectorAll('.tabs-content li');
-    let active;
-    links.forEach(item => {
-        item.addEventListener('click', selectTab);
-        }
-    );
-    function selectTab() {
-        links.forEach(item => {
-            item.classList.remove('active');
-        });
-        this.classList.add('active');
-        active = this.getAttribute('data-tab-name');
-        showContent(active);
-    }
-    function showContent(active) {
-        content.forEach(item => {
-            item.classList.contains(active) ? item.classList.add('content-active') : item.classList.remove('content-active');
-        })
-    }
+let tab = document.querySelectorAll('.tabs-title');
 
-}
+tab.forEach(function(item){
+    item.addEventListener('click', function(){
+        let currentTab = document.querySelector(`.tab-content[data-tab-content="${item.dataset.tab}"]`);
 
-tab();
+        document.querySelector('.tab-content.content-active').classList.remove('content-active');
+        document.querySelector('.tabs-title.active').classList.remove('active');
+
+        currentTab.classList.add('content-active');
+        item.classList.add('active');
+    });
+});
