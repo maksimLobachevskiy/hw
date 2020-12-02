1c0d7951697df9e7bc6f6fbc91949f671a5f42fb
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2020-12-02 19:02:05 +0200

    JS HW8

--- JS HW8/js/main.js
+++ JS HW8/js/main.js
@@ -2,24 +2,24 @@ const priceInput = document.querySelector('#price');
 const span = document.createElement('span');
 span.classList.add('price-value');
 const errorText = document.createElement('p');
-priceInput.addEventListener('focus', (event) => {
-    event.target.style.border = '3px solid darkgreen';
+priceInput.addEventListener('focus', () => {
+    priceInput.style.border = '3px solid darkgreen';
 });
 
-priceInput.addEventListener('blur', (event) => {
+priceInput.addEventListener('blur', () => {
 
     if (priceInput.value < 0 || priceInput.value === "") {
-        event.target.style.border = '3px solid red';
+        priceInput.style.border = '3px solid red';
         document.querySelector('label').after(errorText);
         errorText.innerText = 'Please enter correct price.';
         span.remove();
     } else {
         document.querySelector('label').before(span);
         span.innerHTML = `Текущая цена: ${priceInput.value} <button class="close-btn">X</button>`;
-        event.target.style.color = 'green';
+        priceInput.style.color = 'green';
         errorText.remove();
         const closeBtn = document.querySelector('.close-btn');
-        closeBtn.addEventListener('click', (event) => {
+        closeBtn.addEventListener('click', () => {
             span.remove();
             priceInput.value = "";
         });
