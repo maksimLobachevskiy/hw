037dc69296ac3561e0e12cd64645eb930abe3cdc
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2020-12-04 20:56:54 +0200

    JSHW10 (unify)

--- jsHw10/js/main.js
+++ jsHw10/js/main.js
@@ -1,45 +1,37 @@
-const password = document.getElementById('pass');
-const repeatPass = document.getElementById('repeat_pass');
-function validatePassword() {
-    const icon = document.getElementById('icon1');
-    const icon2 = document.getElementById('icon2');
-    icon.addEventListener('click', () => {
-        if (password.getAttribute('type') === 'password') {
-            password.setAttribute('type', 'text');
-            icon.classList.add('fa-eye-slash');
-            icon.classList.remove('fa-eye');
-        } else {
-            icon.classList.remove('fa-eye-slash');
-            icon.classList.add('fa-eye');
-            password.setAttribute('type', 'password');
-        }
-    });
+const icon = document.getElementById('icon1');
+const icon2 = document.getElementById('icon2');
+function showPass(target, icon) { // create a function that accepts arguments
+    const element = document.getElementById(target) // query whatever element you pass to it.
+    if (element.getAttribute('type') === 'password') {
+        element.setAttribute('type', 'text');
+        icon.classList.add('fa-eye-slash');
+        icon.classList.remove('fa-eye');
+    } else {
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+        element.setAttribute('type', 'password');
+    }
+}
 
-    icon2.addEventListener('click', () => {
-        if (repeatPass.getAttribute('type') === 'password') {
-            repeatPass.setAttribute('type', 'text');
-            icon2.classList.add('fa-eye-slash');
-            icon2.classList.remove('fa-eye');
-        } else {
-            icon2.classList.remove('fa-eye-slash');
-            icon2.classList.add('fa-eye');
-            repeatPass.setAttribute('type', 'password');
-        }
-    });
+icon.addEventListener('click', () => showPass('pass', icon)); // add the event listeners wrapped in anonymous function so it's not called immediately
+icon2.addEventListener('click', () => showPass('repeat_pass', icon2));
 
+function validatePass() {
+    const password = document.getElementById('pass');
+    const repeatPass = document.getElementById('repeat_pass');
     const submit = document.querySelector('.password-form')
     const errorText = document.createElement('span');
     submit.addEventListener('submit', () => {
             if (password.value !== repeatPass.value) {
                 repeatPass.after(errorText);
-                errorText.innerText = 'Нужно ввести одинаковые значения.';
+                errorText.innerText = 'Пароли не совпадают.';
                 errorText.style.color = 'red';
             } else {
+                errorText.remove();
                 password.value = "";
                 repeatPass.value = "";
                 alert('You are welcome');
             }
-        }
-    );
+        });
 }
-validatePassword();
+validatePass()
